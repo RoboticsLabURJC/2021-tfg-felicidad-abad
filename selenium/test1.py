11d8a6ae7b3ad0aaa251247fa9fd63174ec8b9a9
felicidadaqm <felicidadaqm@gmail.com>
2021-11-19 00:35:41 +0100

    Basic selenium 2, abrir Google + popup

--- selenium/test1.py
+++ selenium/test1.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = r"C:\Users\cafiz\OneDrive\Documentos\Universidad\TFG\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -7,4 +11,9 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://www.google.es/")
 print(driver.title)
 
+## Aceptar el popup
+aceptar = driver.find_element_by_id("L2AGLb")
+aceptar.click()
+
+time.sleep(10)
 driver.quit()
