331466c6d6cb1a5a5b6e3f249b044dfc6621624f
felicidadaqm <felicidadaqm@gmail.com>
2022-05-18 23:33:53 +0200

    Truncado imagen a 0, solución V

---
+++ Ejercicio-Imagen-Soluciones/truncado-to-zero.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('lena.jpg')
+
+## pasamos a gris la imagen
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+_,trunc = cv2.threshold(gray,100,255,cv2.THRESH_TOZERO)
+
+cv2.imshow('thresholding',trunc)
+
+if cv2.waitKey(0):
+    cv2.destroyAllWindows()
