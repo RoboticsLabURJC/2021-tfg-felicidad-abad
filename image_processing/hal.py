9b8aac3772a226b5e37caa5ad3eda0c2f59f5079
felicidadaqm <43533577+felicidadaqm@users.noreply.github.com>
2023-04-24 00:28:26 +0200

    Create hal.py

---
+++ image_processing/hal.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+import logging
+
+class HAL:
+
+    def __init__(self):
+        logging.basicConfig(filename="halpy.log", level=logging.DEBUG)
+        logging.warning("se ejecuta el hal.py")
+        self.current_path = os.path.dirname(os.path.abspath(__file__))
+        self.cameraCapture = cv2.VideoCapture(self.current_path + "/video.mp4")
+        self.frame_number = 0
+
+    def getImage(self, frame_number):
+        self.cameraCapture.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        success, frame = self.cameraCapture.read()
+        return frame
+
